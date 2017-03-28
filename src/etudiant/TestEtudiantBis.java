fec5d0bb325645d963aba92dcb25ebf34bcccffc
StraetenMax <maxstraeten@yahoo.com>
2017-03-28 15:59:59 +0200

    finalisation complete du projet

---
+++ src/etudiant/TestEtudiantBis.java
@@ -0,0 +1,13 @@
+package etudiant;
+
+public class TestEtudiantBis {
+
+	public static void main(String[] arg) {
+		Etudiant etudiant = new Etudiant(arg[0]);
+		etudiant.travailler();
+		etudiant.seReposer();
+		
+
+	}
+
+}
